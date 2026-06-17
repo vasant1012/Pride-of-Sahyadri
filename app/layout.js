0bb337e7d443941a329ca578ed7df968c52daac0
vasant1012 <vasant.s.rajadhyax@gmail.com>
2026-06-17 14:15:55 +0530

    Add favicon and link to layout for improved branding

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
